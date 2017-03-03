db315a08b7f35ddb6e53e1da615269d4922fab67
Visin___Wei <zhuzhanhao1991@gmail.com>
2017-03-02 18:34:17 -0800

    Asset Only
    Asset Only

---
+++ WorldMapEditor/Assets/Editor/WorldMapEditor/WorldMapInspector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using sonil.rpgkit;
+
+namespace sonil.Editors {
+	[CustomEditor(typeof(WorldMapData),true)]
+	public class WorldMapInspector : Editor {
+		public void OnEnable() {
+			EditorApplication.projectWindowItemOnGUI += OnDoubleClick;
+		}
+
+		public void  OnDisable() {
+			EditorApplication.projectWindowItemOnGUI -= OnDoubleClick;
+		}
+
+		public override void OnInspectorGUI (){
+			base.OnInspectorGUI ();
+		}
+
+		protected override void OnHeaderGUI (){
+			base.OnHeaderGUI ();
+		}
+
+		public virtual void OnDoubleClick(string guid,Rect rect){
+			if (Event.current.type == EventType.MouseDown && Event.current.clickCount == 2 
+				&& rect.Contains (Event.current.mousePosition)) {
+				WorldMapEditor.ShowWindow((WorldMapData)target);
+			}
+		}
+	}
+}
---
+++ WorldMapEditor/Assets/WorldMapData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace sonil.rpgkit {
+	[System.Serializable]
+	public class WorldMapData : ScriptableObject {
+		public Texture background;
+	}
+}
